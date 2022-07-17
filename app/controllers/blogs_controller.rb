e2b065ea0149a834960c562687507c2c9d2fa8a4
jrshort89@gmail.com <jakeshort@Jakes-MacBook-Air.local>
2022-07-17 13:28:25 -0400

    added blog controller logic

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -4,17 +4,34 @@ class BlogsController < ApplicationController
   end
 
   def show
-  end
-
-  def new
+    render json: Blog.find(params[:id])
   end
 
   def create
+    blog = Blog.new blog_params
+    if blog.save
+      render json: blog
+      return
+    end
+    render json: "Failed to create blog!"
   end
 
   def update
+    blog = Blog.find params[:id]
+    blog.update blog_params
+    render json: blog
   end
 
-  def delete
+  def destroy
+    blog = Blog.find params[:id]
+    if blog.destroy
+      render json: "Successfully deleted blog #{params[:id]}!"
+    end
+  end
+
+  private
+
+  def blog_params
+    params.require(:blog).permit(:title, :body)
   end
 end
