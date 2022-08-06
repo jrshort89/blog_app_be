64e694e1343854d7447f3e97c897ddf37955abda
jrshort89@gmail.com <jakeshort@Jakes-MacBook-Air-2.local>
2022-08-06 11:29:34 -0400

    add user model tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it 'creates' do
+  it 'creates with attributes' do
     user = build(:user)
     expect(user.first_name).to eq('John')
+    expect(user.last_name).to eq('Doe')
+    expect(user.email).to eq('test@test.com')
+    expect(user.password).to eq('password')
+    expect(user.save).to be(true)
+  end
+
+  it 'does not create with missing email' do
+    user = build(:user, email: '')
+    expect(user.save).to be(false)
+  end
+
+  it 'does not create with invalid email' do
+    user = build(:user, email: 'not an email')
+    expect(user.save).to be(false)
   end
 end
