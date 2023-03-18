66f61d60f4b2a0c80bb0377d3955ea4041a56b6e
jrshort89@gmail.com <jakeshort@Jakes-MacBook-Air-2.local>
2023-03-18 15:47:32 -0400

    add spanish_translation_group mutation handling for arrays

--- app/graphql/mutations/translate/spanish_translation_group_create.rb
+++ app/graphql/mutations/translate/spanish_translation_group_create.rb
@@ -5,13 +5,14 @@ module Mutations
     class SpanishTranslationGroupCreate < BaseMutation
       description "Creates a new spanish_translation_group"
 
-      field :spanish_translation_group, Types::SpanishTranslationGroupType, null: false
+      field :spanish_translation_group, [Types::SpanishTranslationGroupType], null: false
 
       argument :spanish_translation_group_input, Types::Input::SpanishTranslationGroupInputType, required: true
 
       def resolve(spanish_translation_group_input:)
-        spanish_translation_group = ::SpanishTranslationGroup.new(**spanish_translation_group_input)
-        raise GraphQL::ExecutionError.new "Error creating spanish_translation_group", extensions: spanish_translation_group.errors.to_hash unless spanish_translation_group.save
+        spanish_translation_group = spanish_translation_group_input.spanish_translation_id.map { |spanish_translation_id| ::SpanishTranslationGroup.new(group_id: spanish_translation_group_input.group_id, spanish_translation_id: spanish_translation_id) }
+
+        raise GraphQL::ExecutionError.new "Error creating spanish_translation_group", extensions: spanish_translation_group.errors.to_hash unless spanish_translation_group.each { |stg| stg.save }
 
         { spanish_translation_group: spanish_translation_group }
       end
--- app/graphql/types/input/spanish_translation_group_input_type.rb
+++ app/graphql/types/input/spanish_translation_group_input_type.rb
@@ -4,7 +4,7 @@ module Types::Input
   class SpanishTranslationGroupInputType < Types::BaseInputObject
     argument :id, ID, required: false
     argument :group_id, Integer, required: false
-    argument :spanish_translation_id, Integer, required: false
+    argument :spanish_translation_id, [Integer], required: false
     argument :created_at, GraphQL::Types::ISO8601DateTime, required: false
     argument :updated_at, GraphQL::Types::ISO8601DateTime, required: false
   end
